a65c34e637afefcced2228a17b69020192679031
luiz.bezerra <luizantoniopcb@gmail.com>
2024-10-19 01:03:29 -0300

    Carousel create, card-sale create, creating pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CardSaleComponent } from './card-sale/card-sale.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CarouselComponent, CardSaleComponent, PaginationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/card-sale/card-sale.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-sale',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-sale.component.html',
+  styleUrl: './card-sale.component.css'
+})
+export class CardSaleComponent {
+
+}
