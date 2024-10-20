ac949d3a08a533ff387ba092aabff83f4253e4a2
luiz.bezerra <luizantoniopcb@gmail.com>
2024-10-20 00:15:46 -0300

    card-category created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CardSaleComponent } from './card-sale/card-sale.component';
 import { PaginationComponent } from './pagination/pagination.component';
+import { CardCategoryComponent } from './card-category/card-category.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarouselComponent, CardSaleComponent, PaginationComponent],
+  imports: [RouterOutlet, HeaderComponent, CarouselComponent, CardSaleComponent, PaginationComponent, CardCategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
