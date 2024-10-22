afb282a9dcb093e17d87a63a425a5b89187c1cd5
luiz.bezerra <luizantoniopcb@gmail.com>
2024-10-21 22:48:36 -0300

    Otimização função de quantidades de produtos

--- src/app/card-product/card-product.component.ts
+++ src/app/card-product/card-product.component.ts
@@ -6,39 +6,27 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './card-product.component.html',
-  styleUrls: ['./card-product.component.css']
+  styleUrls: ['./card-product.component.css'],
 })
 export class CardProductComponent {
   @Input() nomeProduto!: string;
   @Input() quantidadeProduto!: number;
   @Input() precoProduto!: number;
 
-  isClickedRemove = false;
-  isClickedAdd = false;
+  isClicked: 'remove' | 'add' | null = null;
+  quantidade: number = 1;
 
-  quantidade = 1;
+  toggleClick(buttonId: 'remove' | 'add'): void {
+    this.isClicked = (this.isClicked === buttonId) ? null : buttonId;
 
-  toggleClick(buttonId: string) {
-    if (buttonId == 'remove') {
-      this.isClickedRemove = !this.isClickedRemove;
-      this.isClickedAdd = false;
-
-      if (this.quantidade > 1) {
-        this.quantidade -= 1
-      }
-
-      setTimeout(() => {
-        this.isClickedRemove = !this.isClickedRemove;
-      }, 500)
+    if (buttonId === 'remove' && this.quantidade > 1) {
+      this.quantidade -= 1;
     } else if (buttonId === 'add') {
-      this.isClickedAdd = !this.isClickedAdd;
-      this.isClickedRemove = false;
-
       this.quantidade += 1
-
-      setTimeout(() => {
-        this.isClickedAdd = !this.isClickedAdd;
-      }, 500)
     }
+
+    setTimeout(() => {
+      this.isClicked = null;
+    }, 500);
   }
 }
