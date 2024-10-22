89a67c1a56905056409bba131f2a993c43534b5d
luiz.bezerra <luizantoniopcb@gmail.com>
2024-10-22 00:30:49 -0300

    Organizado pasta components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { CardSaleComponent } from './components/card-sale/card-sale.component';
-import { PaginationComponent } from './pagination/pagination.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 import { CardCategoryComponent } from './components/card-category/card-category.component';
 import { CardProductComponent } from './components/card-product/card-product.component';
 
