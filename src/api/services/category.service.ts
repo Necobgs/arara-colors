fa5b2102a7e552fd4926ec249dc7e148d654544a
Necobgs <emanuelborgesdev@gmail.com>
2024-11-05 00:45:35 -0300

    css in features product add

--- src/api/services/category.service.ts
+++ src/api/services/category.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 import { Category } from '../models/interfaces/category';
 
 @Injectable({
@@ -18,4 +18,3 @@ export class CategoryService {
   }
 
 }
-  
